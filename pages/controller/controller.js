fbb5a4284155531bcc0bc24f6232954bf0fecc3d
rainhon <240918304@qq.com>
2021-11-08 10:31:22 +0800

    stick

--- pages/controller/controller.js
+++ pages/controller/controller.js
@@ -1,31 +1,33 @@
 // pages/controller.js
-let controlWidth, controlHeight
-
 Page({
   data: {
     x: 0,
     y: 0,
+    stickWidth: '10rpx',
+    stickHeight: '10rpx',
+    areaWidth: 0,
+    areaHeight: 0,
   },
   onReady: function () {
-    let _this = this
-    console.log(this)
     wx.createSelectorQuery().
-    select('.control-area').boundingClientRect(function(res){
-      controlWidth = res.width
-      controlHeight = res.height
-      _this.setData({
-        x: controlWidth/2,
-        y: controlHeight/2
+    select('.control-area').boundingClientRect((res)=>{
+      let width = res.width
+      let height = res.height
+      this.setData({
+        x: width/2,
+        y: height/2,
+        areaWidth:width,
+        areaHeight:height,
       })
+      // console.log(this.x)
     }).exec()
     // let controlAreaWidth=
   },
 
   resetController: function(){
-    console.log(this)
     this.setData({
-      x: controlWidth/2,
-      y: controlHeight/2
+      x: this.data.areaWidth/2,
+      y: this.data.areaHeight/2
     })
   }
 })
